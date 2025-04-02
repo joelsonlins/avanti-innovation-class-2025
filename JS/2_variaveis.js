01f25d24034c5eda7c3e31b617c1851c6cb745ce
joelsonlins <joelsonlins@hotmail.com>
2025-04-02 00:29:41 -0300

    JS Aula 05 - tipo de dado number

--- JS/2_variaveis.js
+++ JS/2_variaveis.js
@@ -1,6 +1,12 @@
-const nome = "Paulo"
+const nome = "Paulo";
 let sobrenome = "da Silva";
-const firstName = "Maria"
-const lastName = "Pereira" 
-console.log(nome + " " + sobrenome)
-console.log(`Meu nome é ${firstName} ${lastName}`)
+const firstName = "Maria";
+const lastName = "Pereira";
+const idade = 40;
+
+const valor1 = 2.10;
+const valor2 = 3.20;
+
+console.log(`O valor total é: ${valor1 + valor2}`)
+console.log(nome + " " + sobrenome);
+console.log(`Meu nome é ${firstName} ${lastName} e minha idade é ${idade}`);
