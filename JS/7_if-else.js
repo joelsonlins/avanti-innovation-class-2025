a0913949fc9e4d1e0df97be72c0c9773c992151f
joelsonlins <joelsonlins@hotmail.com>
2025-04-03 22:15:38 -0300

    JS - Aula 15 - if else

---
+++ JS/7_if-else.js
@@ -0,0 +1,18 @@
+const notaFinal = 6;
+
+const frequencia = 6;
+
+if(notaFinal>=7 && frequencia>=8){
+  console.log("Aprovado");
+}else{
+  console.log("Reprovado");
+}
+
+
+// if (notaFinal>= 7){
+//   console.log("Aprovado")
+// }else if(notaFinal>=6){
+//   console.log("Recuperação")
+// }else{
+//   console.log("Reprovado")
+// }
