278a51b915f34a3dab93d2112d53d0fbad01ee9f
joelsonlins <joelsonlins@hotmail.com>
2025-04-03 21:36:13 -0300

    JS - Aula 6 e 7 - dados booleanos e comentários

--- JS/2_variaveis.js
+++ JS/2_variaveis.js
@@ -7,6 +7,12 @@ const idade = 40;
 const valor1 = 2.10;
 const valor2 = 3.20;
 
+//const isLogin = true;
+
+
+/*
 console.log(`O valor total é: ${valor1 + valor2}`)
 console.log(nome + " " + sobrenome);
 console.log(`Meu nome é ${firstName} ${lastName} e minha idade é ${idade}`);
+
+*/
