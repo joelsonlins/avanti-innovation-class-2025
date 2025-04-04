0ac8c8ace12a8f1adbc10fae2d6a963f72887342
joelsonlins <joelsonlins@hotmail.com>
2025-04-03 21:49:09 -0300

    JS - Aula 11 - operadores aritméticos

---
+++ JS/3_operadores-aritmeticos.js
@@ -0,0 +1,17 @@
+const numero01 =  5;
+const numero02 = 2;
+const numero03 = 3;
+
+
+let resultado = numero01 + numero02;
+console.log(`O resultado é da soma de ${numero01} e ${numero02}: ${resultado}`)
+resultado = numero01 - numero02;
+console.log(`O resultado é da subtração de ${numero01} e ${numero02}: ${resultado}`)
+resultado = numero01 / numero02;
+console.log(`O resultado é da divisão de ${numero01} e ${numero02}: ${resultado}`)
+resultado = numero01 * numero02;
+console.log(`O resultado é da multiplicação de ${numero01} e ${numero02}: ${resultado}`)
+resultado = numero01%numero02;
+console.log(`O resultado é do resto da divisão de ${numero01} e ${numero02}: ${resultado}`)
+resultado = numero01**numero02;
+console.log(`O resultado é da potenciação de ${numero01} e ${numero02}: ${resultado}`)
