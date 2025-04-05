15485c6f1b45d000fcd9dccf1455538e7337ef9f
joelsonlins <joelsonlins@hotmail.com>
2025-04-05 07:40:58 -0300

    JS - Aula 30 - DOM - getElementByid, innerHTML, style

---
+++ JS/DOM-selecao/js/scripts.js
@@ -0,0 +1,9 @@
+// seleção por ID
+
+const content = document.getElementById("content")
+
+console.log("content", content)
+
+content.innerHTML = `<p>Parágrafo de texto</p>`
+content.style.backgroundColor ="#F00"
+content.style.fontSize="30px"
