d993d32575d3cfdb2ad69bca52c597fcb4b5c40a
joelsonlins <joelsonlins@hotmail.com>
2025-04-04 22:13:07 -0300

    JS - Aula 21 - map

---
+++ JS/13_map.js
@@ -0,0 +1,19 @@
+const valores = [2,3,6,9]
+
+
+
+const valoresDobrado = valores.map(valor => valor * 2);
+
+// console.log(valoresDobrado)
+
+// console.log(valores)
+
+const users=[
+  {nome:"Luciano", age:40},
+  {nome:"Pedro", age:15},
+  {nome:"Maria", age:54}
+]
+
+const userNames = users.map(user => user.nome)
+
+console.log(userNames)
