ef365a5b69a5b9c00d49eeb5e27eaba3b2538a5c
joelsonlins <joelsonlins@hotmail.com>
2025-04-05 08:01:20 -0300

    JS - Aula 31 - DOM - getElementsByClassName, htmlCollection

--- JS/DOM-selecao/js/scripts.js
+++ JS/DOM-selecao/js/scripts.js
@@ -7,3 +7,13 @@ console.log("content", content)
 content.innerHTML = `<p>Parágrafo de texto</p>`
 content.style.backgroundColor ="#F00"
 content.style.fontSize="30px"
+
+// seleção por classe
+
+const classText = document.getElementsByClassName("text")
+
+console.log("classtext", classText)
+
+for(text of classText){
+  text.style.color="blue"
+}
