4943603db0695c0c731be4d2568c347deb012d10
joelsonlins <joelsonlins@hotmail.com>
2025-04-04 22:02:31 -0300

    JS - Aula 19 - Escopo

---
+++ JS/11_escopo.js
@@ -0,0 +1,22 @@
+// let minhaVariavelGlobal = "Eu sou uma variavel global"
+
+// function minhaFuncao(){
+//   console.log(minhaVariavelGlobal)
+// }
+
+// minhaFuncao()
+
+// function escopo(){
+//   let minhaVariavellocal = "Eu sou uma variável local"
+//   console.log(minhaVariavellocal)
+// }
+
+// escopo()
+// console.log(minhaVariavellocal)
+
+if(true){
+  let minhaVariavellocal = "variável local"
+  console.log(minhaVariavellocal)
+}
+
+console.log(minhaVariavellocal)
