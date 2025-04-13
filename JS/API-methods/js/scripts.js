0314deab8b1c6e858071033366035ec09a958e0c
joelsonlins <joelsonlins@hotmail.com>
2025-04-13 14:58:10 -0300

    JS - Aula 47 - Requisições PATCH

--- JS/API-methods/js/scripts.js
+++ JS/API-methods/js/scripts.js
@@ -31,13 +31,27 @@
 
 // -- PUT
 
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//   method: 'PUT',
+//   body: JSON.stringify({
+//     id: 1,
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// -- PATCH
+
 fetch('https://jsonplaceholder.typicode.com/posts/1', {
-  method: 'PUT',
+  method: 'PATCH',
   body: JSON.stringify({
-    id: 1,
     title: 'foo',
-    body: 'bar',
-    userId: 1,
   }),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
