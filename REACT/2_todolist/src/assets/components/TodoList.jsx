a63f050eb9ec05b8bb82423aa90fe06903b7126d
joelsonlins <joelsonlins@hotmail.com>
2025-04-23 07:56:21 -0300

    Reat - Aula 19 - Projeto todolist - estrutura

--- REACT/2_todolist/src/assets/components/TodoList.jsx
+++ REACT/2_todolist/src/assets/components/TodoList.jsx
@@ -3,7 +3,23 @@ import styles from "./TodoList.module.css"
 
 function TodoList() {
   return (
-    <div>TodoList</div>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Lista de Tarefas</h2>
+      <div className={styles.inputContaienr}>
+        <input 
+          type="text"
+          placeholder='Adicione uma tarefa'
+          className={styles.input}
+        />
+        <button className={styles.button}>Adiconar</button>
+
+      </div>
+      <ul className={styles.taskList}>
+        <li className={styles.taskItem}>Tarefa</li>
+
+      </ul>
+
+    </div>
   )
 }
 
